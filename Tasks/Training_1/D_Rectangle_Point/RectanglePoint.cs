26d92c00b5155df01540baa400a991c948d8b406
Vladimir Rybalko <vladimir.a.rybalko@gmail.com>
2016-09-17 09:31:40 +0700

    Added sekeleton for 1D task

---
+++ Tasks/Training_1/D_Rectangle_Point/RectanglePoint.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace Tasks
+{
+    public class Expression : CompetitiveTask
+    {
+        private const string Training = @"Training_1/D_Rectangle_Point/";
+
+        public override string InputFile { get; } = Training + @"tria-pt.in";
+        public override string OutputFile { get; } = Training + @"tria-pt.out";
+		               
+        protected override void Resolve(StreamReader reader, StreamWriter writer)
+        {
+        }        
+    }
+}
