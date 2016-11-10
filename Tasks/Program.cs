86eb00cbc1e3465b6c972130ff7e6a0ac48ec6bb
Vladimir Rybalko <vladimir.a.rybalko@gmail.com>
2016-11-10 11:29:46 +0700

    Resolve: 2A_Prime Numbers

--- Tasks/Program.cs
+++ Tasks/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            new Power().Resolve();
+            new PrimesNumber2().Resolve();
         }
     }
 }
--- Tasks/Training_2/A_PrimesNumber2/PrimesNumber2.cs
+++ Tasks/Training_2/A_PrimesNumber2/PrimesNumber2.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Tasks
 {
@@ -11,37 +13,70 @@ namespace Tasks
 
         protected override void Resolve(StreamReader reader, StreamWriter writer)
         {
-            //var m = reader.ReadInt();
-            //var n = reader.ReadInt();
+            const int maxSqrtConstraint = 1000; // task constraints
 
-            //var existPrimes = false;
-            //for (var i = m; i <= n; i++)
-            //{
-            //    if (IsPrime(i))
-            //    {
-            //        existPrimes = true;
-            //        writer.Write(i + " ");
-            //    }
-            //}
+            var m = reader.ReadInt();
+            var n = reader.ReadInt();
 
-            //if (!existPrimes)
-            //    writer.Write("Absent");
+            // find all primes from 1 to maximum sqrt constraint
+            var primes = new List<int>();
+            for (var i = 2; i < maxSqrtConstraint; i++)
+                if (IsPrime(i))
+                    primes.Add(i);
 
+            // find sqrt for all numbers from m to n
+            var sqrts = new int[n - m + 1];
+            sqrts[0] = (int)Math.Floor(Math.Sqrt(m));
+            var j = 1;
+            for (var number = m + 1; number <= n; number++)
+            {
+                var sqr = sqrts[j - 1] + 1;
+                sqrts[j] = (sqr * sqr == number) 
+                    ? sqrts[j - 1] + 1
+                    : sqrts[j - 1];
+                j++;
+            }
+
+            // find all primes numbers from m to n
+            j = 0;
+            var existPrimes = false;
+            for (var number = m; number <= n; number++)
+            {
+                var primesBeforeSqrt = primes.Where(x => x <= sqrts[j]).ToArray();
+
+                if (IsPrime(number, primesBeforeSqrt))
+                {
+                    existPrimes = true;
+                    writer.WriteLine(number);
+                }
+
+                j++;
+            }
+
+            if (!existPrimes)
+                writer.Write("Absent");
         }
-
-        // Detect all prime numbers from statement range
-        //private static int[] DetectAllPrimeNumbers()
-        //{
-
-
-        //}
-        
+                
         private static bool IsPrime(int number)
         {
             var i = 2;
             while (i <= Math.Sqrt(number))
             {
                 if (number % i == 0)
+                    return false;
+                i++;
+            }
+
+            return true;
+        }
+
+
+        private static bool IsPrime(int number, int[] primes)
+        {
+            var i = 0;
+            while (i < primes.Length)
+            {
+                if (number % primes[i] == 0)
                     return false;
                 i++;
             }
